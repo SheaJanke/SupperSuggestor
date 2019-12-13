6ace98b4e461ee92a0742953ee02a19fad60c06e
SheaJanke <sheajanke@gmail.com>
2019-12-13 10:31:57 -0800

    Changing color button.

---
+++ script.js
@@ -0,0 +1,14 @@
+function onoff(){
+    currentvalue = document.getElementById('onoff').value;
+    if(currentvalue == "Off"){
+    document.getElementById("onoff").value="On";
+    document.getElementById('onoff').style.backgroundColor = "#33A1FD";
+    }else{
+    document.getElementById("onoff").value="Off";
+    document.getElementById('onoff').style.backgroundColor = "#054A29";
+    }
+}
+
+function changeColor(){
+    document.getElementById('onoff').value;
+}
